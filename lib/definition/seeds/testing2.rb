4a1fb98c837de369da3d9b8e31a5b6e501f1322e
Atsuya <atsuya@puppetlabs.com>
2012-07-26 14:09:29 -0700

    completed EL family seeds

--- lib/definition/seeds/testing2.rb
+++
@@ -1,7 +0,0 @@
-### using system([env,] command [,options] to use seed_builder.rb
-require 'yaml'
-list = YAML.load_file('/Users/atsuya/Code/ERB/alpha/el_perms.yml')
-list.each do |type|
-  puts "Creating seed for #{type} !!!"
-  system('ruby', 'seed_builder.rb', type)
-end
