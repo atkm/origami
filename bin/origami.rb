3609e596f9f6bb45f7eae4111aa5cdc7f48fa7e1
Atsuya <atsuya@puppetlabs.com>
2012-07-26 19:42:53 -0700

    modification to work with seisan

--- bin/origami.rb
+++ bin/origami.rb
@@ -6,67 +6,71 @@
 ###        origami.rb --file <file.yml> [<target directory>] :: where <file.yml> contains an array of names
 
 project_path = File.expand_path(
-                    File.join(File.dirname(__FILE__), '..', 'lib'))
+                                File.join(File.dirname(__FILE__), '..', 'lib'))
+
+origami_bin = File.expand_path(File.join( project_path, 'origami/bin/origami.rb'))
 # puts project_path
 $LOAD_PATH.unshift(project_path) unless $LOAD_PATH.include?(project_path)
 
 require 'build_from_seed'
 require 'optparse'
 
-if ARGV.length == 0
-  piece_of_art = File.new(File.join(File.dirname(__FILE__), 'hello.txt'),'r').read
-  print( piece_of_art + "\n")
-  print("'origami --help' for usage\n\n")
-else
-  names = [ARGV[0]]
-  
-  options = {}
-  optparse = OptionParser.new do |opts|
-    opts.banner = "Usage: origami.rb name [options]"
+if __FILE__ == $0
+  if ARGV.length == 0
+    piece_of_art = File.new(File.join(File.dirname(__FILE__), 'hello.txt'),'r').read
+    print( piece_of_art + "\n")
+    print("'origami --help' for usage\n\n")
+  else
+    names = [ARGV[0]]
     
-    options[:target] = nil
-    opts.on('--target DIR') do |dir|
-      options[:target] = dir
-    end
-    
-    options[:instruction] = nil
-    opts.on('--definition') do
-      options[:instruction] = 'definition'
-    end
-    opts.on('--kickstart') do
-      options[:instruction] = 'kickstart'
-    end
+    options = {}
+    optparse = OptionParser.new do |opts|
+      opts.banner = "Usage: origami.rb name [options]"
+      
+      options[:target] = nil
+      opts.on('--target DIR') do |dir|
+        options[:target] = dir
+      end
+      
+      options[:instruction] = nil
+      opts.on('--definition') do
+        options[:instruction] = 'definition'
+      end
+      opts.on('--kickstart') do
+        options[:instruction] = 'kickstart'
+      end
 
-    options[:file] = nil
-    opts.on('--file FILE') do |file|
-      options[:file] = file
+      options[:file] = nil
+      opts.on('--file FILE') do |file|
+        options[:file] = file
+      end
+      
+      opts.on('-h','--help') do
+        puts opts
+        exit
+      end
+      
     end
     
-    opts.on('-h','--help') do
-      puts opts
-      exit
+    optparse.parse!
+
+    target = options[:target]
+
+    if options[:file] != nil
+      require 'yaml'
+      names = YAML.load_file(options[:file])
     end
     
-  end
-  
-  optparse.parse!
-
-  target = options[:target]
-
-  if options[:file] != nil
-    require 'yaml'
-    names = YAML.load_file(options[:file])
-  end
-  
-  names.each do |name|
-    if options[:instruction] == nil
-      build_from_seed(name,'kickstart', target)
-      puts
-      build_from_seed(name,'definition', target)
-      puts
-    else
-      build_from_seed(name, options[:instruction], target)
-      puts
+    names.each do |name|
+      if options[:instruction] == nil
+        build_from_seed(name,'kickstart', target)
+        puts
+        build_from_seed(name,'definition', target)
+        puts
+      else
+        build_from_seed(name, options[:instruction], target)
+        puts
+      end
     end
   end
 end
