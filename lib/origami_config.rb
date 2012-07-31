25690d6a56d8749f638b66c0076efa8b00c1590e
Atsuya <atsuya@puppetlabs.com>
2012-07-31 16:44:45 -0700

    modify origami_config

--- lib/origami_config.rb
+++ lib/origami_config.rb
@@ -1,11 +1,15 @@
 ### Directory containing ks_base.erb and seeds/ directory. The same for definition.
 module Origami
-  
+  def project_dir
+    return File.expand_path(
+                            File.join(File.dirname(__FILE__),'../lib')
+                            )
+  end 
   def ks_dir
-    return File.expand_path("~/Code/origami/lib/kickstart/")
+    return project_dir + '/kickstart/'
   end
   def defn_dir
-    return File.expand_path("~/Code/origami/lib/definition/")
+    return project_dir + '/definition/'
   end
   
 end
